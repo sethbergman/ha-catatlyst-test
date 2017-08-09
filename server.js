90319f3af34d0dd762d1eaa7eb88935d3e8a8a85
Seth Bergman <seth.atxwebs@gmail.com>
2017-08-09 17:13:15 -0500

    ehhh

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require ('express');
 const app = express();
 const http = require('http');
+const sqrt = require('math-sqrt');
+
+require('math');
+
 app.engine('html', require('ejs').renderFile);
 app.set('/views', express.static(__dirname + '/views'))
 app.set('view engine', 'html');
 
 app.get('/', function (req, res){
 	res.render('index.html');
+})
+
+app.get('/math', function (req, res) {
+	res.render('math.html')
 })
 
 const server = app.listen(process.env.PORT || 5000, function () {
---
+++ views/math.js
@@ -0,0 +1,8 @@
+var sqrt = require('math-sqrt');
+
+Math.sqrt(2);
+Date.now(2);
+
+// Math.sqrt(1);  // 1
+// Math.sqrt(0);  // 0
+// Math.sqrt(-1); // NaN
