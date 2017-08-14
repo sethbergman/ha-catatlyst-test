30a5cb93c3c111999b5037e9775c1cb942377986
Seth Bergman <seth.atxwebs@gmail.com>
2017-08-13 21:58:22 -0500

    Update math.js

--- views/math.js
+++ views/math.js
@@ -1,15 +1,7 @@
-import * from 'math-sqrt';
-export class Math {
-Math.sqrt(2).then({Date.now()})
-window.alert(expect(2).toBe(Math.[...Array(length).keys()]);)
-}
-
-var sqrt = require('math-sqrt');
-
-Math.sqrt(2);
-Date.now(2);
-
-// Math.sqrt(1);  // 1
-// Math.sqrt(0);  // 0
-// Math.sqrt(-1); // NaN
-\forall x \geq 0, \mathtt{Math.sqrt(x)} = \sqrt{x} = \text{the unique} \; y \geq 0 \; \text{such that} \; y^2 = x
+const sqrt = require("math-sqrt")
+let value = (x) => sqrt()
+let x = Date.now()
+let y = (sqrt(x))
+console.log(x)
+console.log(y)
+sqrt(y)
