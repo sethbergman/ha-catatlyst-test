fba285cab3a4fd4fb9ea7cf69d93c244397dc333
Seth Bergman <seth.atxwebs@gmail.com>
2017-08-13 23:14:27 -0500

    save

--- src/math.js
+++ src/math.js
@@ -1,11 +1,14 @@
-// import math-sqrt from 'math';
+import math-sqrt from 'math';
+import {inject, customAttribute, DOM} from 'aurelia-framework';
 export class Math {
-	import math-sqrt from 'math';
+@customAttribute(value)
+@inject(DOM.Element)
 	constructor()
-	let value = (x) => sqrt()
-	let x = Date.now()
-	let y = (sqrt(x))
-	console.log(x)
-	console.log(y)
-	sqrt(y)
+		let value = (x) => sqrt()
+		let x = Date.now()
+		let y = (sqrt(x))
+		console.log(x)
+		console.log(y)
+		sqrt(y)
+	}
 }
